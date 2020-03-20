72f90a73dd2ee32e46884ad29b738964f84bbaac
Eslam Genedy <eslamgenedy377@yahoo.com>
2020-03-20 04:45:41 +0200

    Creating Forms

---
+++ clientserver/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Login from './components/Login'
+import Reply from './components/reply'
+import './App.css';
+import { Row , Col } from 'antd';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Row>
+      <Col span={12} offset={6}>
+        <Login></Login>
+        <Reply></Reply>
+      </Col>
+    </Row>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ clientserver/src/components/Signup.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
+
+const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+  
+  
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+  
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+
+class Signup extends Component {
+   
+    render(){
+        return(
+         <div className="Login">
+             <div class="Container">
+             <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+        <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+         </div>   
+         </div>
+        );
+    }
+}
+
+export default Signup ; 
